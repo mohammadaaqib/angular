7c27b8c1810ffe72ad9da2631fe03cb85cd29418
Mohammad Aaqib <mohammadaaqib77@gmail.com>
2018-04-16 18:46:24 +0500

    inintail code

---
+++ oshop/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBPZbm95LS3STt-C9vh3zkAEb01YkZ30fk",
+    authDomain: "moshproject-81a4c.firebaseapp.com",
+    databaseURL: "https://moshproject-81a4c.firebaseio.com",
+    projectId: "moshproject-81a4c",
+    storageBucket: "",
+    messagingSenderId: "241568295025"
+  }
+};
